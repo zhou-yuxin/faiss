f1d20a520c7eb8c3eec03635ece83424744bc1db
zhou-yuxin <504849766@qq.com>
2020-03-17 01:25:32 -0400

    optimize bfp16.h

--- impl/bfp16.h
+++ impl/bfp16.h
@@ -7,13 +7,15 @@ class BFP16 {
 private:
     uint16_t storage;
 
-    struct Memory {
-        union {
-            float fp32;
-            uint32_t u32;
-            uint16_t u16s[2];
+    typedef union {
+        float fp32;
+        struct {
+            uint32_t mantissa : 23;
+            uint32_t exponent : 8;
+            uint32_t sign : 1;
         };
-    };
+        uint16_t u16s[2];
+    } Formater;
 
 public:
     inline BFP16 () {
@@ -24,23 +26,23 @@ public:
     }
 
     inline void operator = (float x) {
-        Memory m;
-        m.fp32 = x;
-        if (m.u16s[0] & 0x8000) {
-            Memory m2;
-            m2.fp32 = x;
-            m2.u32 &= 0xff800000;
-            m2.fp32 /= 128.0f;
-            m.fp32 += m2.fp32;
+        Formater f;
+        f.fp32 = x;
+        if (f.u16s[0] & 0x8000) {
+            Formater g;
+            g.fp32 = x;
+            g.mantissa = 0;
+            g.fp32 /= 128.0f;
+            f.fp32 += g.fp32;
         }
-        storage = m.u16s[1];
+        storage = f.u16s[1];
     }
 
     inline operator float () const {
-        Memory m;
-        m.u16s[0] = 0;
-        m.u16s[1] = storage;
-        return m.fp32;
+        Formater f;
+        f.u16s[0] = 0;
+        f.u16s[1] = storage;
+        return f.fp32;
     }
 
     inline uint16_t raw() const {
